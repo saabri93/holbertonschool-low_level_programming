b0270c2b0afab4ab4c43298d0c3eb59f7064e8aa
saabri93 <96p5d4j75j@privaterelay.appleid.com>
2025-10-23 02:55:01 +0000

    push

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase, then in uppercase
+ * main - Prints the alphabet in lowercase
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char ch;
+	char ch = 'a';
 
-	for (ch = 'a'; ch <= 'z'; ch++)
+	while (ch <= 'z')
+	{
 		putchar(ch);
-	for (ch = 'A'; ch <= 'Z'; ch++)
-		putchar(ch);
+		ch++;
+	}
 	putchar('\n');
-
 	return (0);
 }
