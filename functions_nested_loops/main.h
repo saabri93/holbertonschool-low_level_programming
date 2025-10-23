15e5d63b1501c7f77cec3685827707686c4cd27b
saabri93 <96p5d4j75j@privaterelay.appleid.com>
2025-10-23 03:31:51 +0000

    push

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,12 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
+void times_table(void);
 
 #endif /* MAIN_H */
 
