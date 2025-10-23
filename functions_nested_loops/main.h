9ee580488b56da2740dd116bbace7aaac0e459ed
saabri93 <96p5d4j75j@privaterelay.appleid.com>
2025-10-23 03:13:25 +0000

    push

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,20 +1,6 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-}
+int _putchar(char c);
+
+#endif /* MAIN_H */
