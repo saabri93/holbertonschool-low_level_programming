b05380c30134caecfcbbfaa5dad6efafc77c9227
saabri93 <96p5d4j75j@privaterelay.appleid.com>
2025-10-27 12:20:38 +0000

    push

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,15 +1,6 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * _isdigit - Vérifie si un caractère est un chiffre (0 à 9)
- * @c: le caractère à tester
- *
- * Return: 1 si c est un chiffre, 0 sinon
- */
-int _isdigit(int c)
-{
-	if (c >= '0' && c <= '9')
-		return (1);
-	else
-		return (0);
-}
+int _isdigit(int c);
+
+#endif /* MAIN_H */
