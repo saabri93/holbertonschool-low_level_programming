528ec3a268d82034479a6951e65e79ea3aa50320
saabri93 <96p5d4j75j@privaterelay.appleid.com>
2025-10-27 10:04:38 +0000

    push

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,16 @@
+#include "main.h"
 #include <stdio.h>
-
-int main(void)
+/**
+ * _isupper - Vérifie si un caractère est une majuscule
+ * @c: le caractère à tester
+ *
+ * Return: 1 si c est une majuscule, 0 sinon
+ */
+int _isupper(int c)
 {
-    char ch1 = 'G';
-    char ch2 = 'g';
-
-    printf("%d\n", _isupper(ch1)); // Affiche : 1
-    printf("%d\n", _isupper(ch2)); // Affiche : 0
-
-    return 0;
+    if (c >= 'A' && c <= 'Z')
+        return (1);
+    else
+        return (0);
 }
 
