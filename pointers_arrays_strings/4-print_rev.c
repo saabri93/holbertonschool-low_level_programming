4ed558ca99fc439ddd589b7a2e98e94c64452474
saabri93 <96p5d4j75j@privaterelay.appleid.com>
2025-10-29 04:34:27 +0000

    push

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+int i;
+
+while (s[i] != '\0')
+	{
+	i++;
+	}
+
+	i--;
+
+while (s[i] != '\0')
+	{
+	_putchar(s[i--]);
+	}
+_putchar('\n');
+}
